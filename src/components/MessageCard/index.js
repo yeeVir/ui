49ce64bbdcf4e768bc9d1d3c2b620690e9ed6e06
yee <1666752003@qq.com>
2021-07-04 10:55:21 +0800

    message

---
+++ src/components/MessageCard/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import StyledMessageCard from './style'
+
+function MessageCard({
+    children,
+    avatarSrc,
+    statusText,
+    avatarStatus,
+    ...rest
+}) {
+    return (
+        <StyledMessageCard {...rest}>
+            {children}
+        </StyledMessageCard>
+    )
+}
+
+MessageCard.propTypes = {
+    children: PropTypes.any,
+}
+
+export default MessageCard
